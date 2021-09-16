c410c271f10dc2e525162bf0ad54a8ea9f8fd034
pyeonne <pyeonne@gmail.com>
2021-09-16 15:31:58 +0900

    ✨ Add notice section (Notice line + Promotion)

--- js/main.js
+++ js/main.js
@@ -59,9 +59,57 @@ window.addEventListener(
  */
 // 나타날 요소들(.fade-in) 찾기.
 const fadeEls = document.querySelectorAll('.visual .fade-in');
+// 나타날 요소들을 하나씩 반복해서 처리!
 fadeEls.forEach((fadeEl, index) => {
+    // 각 요소들을 순서대로(delay) 보여지게 함!
     gsap.to(fadeEl, 1, {
         delay: (index + 1) * 0.7, // 0.7, 1.4, 2.1, 2.7
         opacity: 1,
     });
+});
+
+/**
+ * 슬라이드 요소 관리
+ */
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper', {
+    direction: 'vertical', // 수직 슬라이드
+    autoplay: true, // 자동 재생 여부
+    loop: true, // 반복 재생 여부
+});
+new Swiper('.promotion .swiper', {
+    // direction: 'horizontal', 기본값
+    slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    loop: true,
+    autoplay: {
+        delay: 5000, //기본값 3000
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
+        nextEl: '.promotion .swiper-next', // 다음 버튼 선택자
+    },
+});
+
+/**
+ * Promotion 슬라이드 토글 기능
+ */
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', () => {
+    // 슬라이드 영역 숨김 여부를 반댓값으로 할당!
+    isHidePromotion = !isHidePromotion;
+    if (isHidePromotion) {
+        // 숨김 처리!
+        promotionEl.classList.add('hide');
+    } else {
+        // 보임 처리!
+        promotionEl.classList.remove('hide');
+    }
 });
