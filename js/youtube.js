1305cb6a47e30ec0dc5fcc2871d804f44f23af9d
pyeonne <pyeonne@gmail.com>
2021-09-16 22:22:46 +0900

    ✨ Add youtube video section

---
+++ js/youtube.js
@@ -0,0 +1,27 @@
+// Youtube IFrame API를 비동기로 로드합니다.
+var tag = document.createElement('script');
+
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// onYouTubePlayerAPIReady 함수 이름은,
+// Youtube IFrame Player API에서 사용하는 이름이기 때문에,
+// 다르게 지정하면 동작하지 않는다!
+// 그리고 함수는 전역(Global) 등록해야 한다!
+function onYouTubeIframeAPIReady() {
+    // <div id="player"></div>
+    new YT.Player('player', {
+        videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상 ID
+        playerVars: {
+            autoplay: true,
+            loop: true,
+            playlist: 'An6LvWQuj_8', // 반복 재생할 유튜브 영상 ID 목록
+        },
+        events: {
+            onReady: function (event) {
+                event.target.mute(); // 음소거
+            },
+        },
+    });
+}
